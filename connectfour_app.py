43c58f67b2147cc75441ad19ae5618b51be015d4
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-01-14 14:49:16 -0500

    allow hasslerthurston.com to access /move

--- connectfour_app.py
+++ connectfour_app.py
@@ -4,9 +4,53 @@
 # Personal website
 # 12 January 2014
 
-from flask import Flask, request, jsonify, abort, make_response
+from datetime import timedelta
+from flask import Flask, request, jsonify, abort, make_response, current_app
+from functools import update_wrapper
 import board, compute_moves
 app = Flask(__name__)
+
+# from http://flask.pocoo.org/snippets/56/
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 # error handling
 # responses modified from http://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
@@ -32,6 +76,7 @@ def internal_error(error):
 # }
 
 @app.route('/move', methods = ['POST'])
+@crossdomain(origin='http://hasslerthurston.com')
 def make_move():
 	# check to see if JSON exists
 	if not request.json:
